c00f31128c48d6b181648e7dd2c33825a17b1fa8
Leonardo Rafael Pacheco Muñoz <leorafapacheco@gmail.com>
2017-08-23 20:05:41 -0500

    Add project files.

---
+++ P01_WF_TestGitHub/P01_HelloWorld.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="P01_HelloWorld.aspx.cs" Inherits="P01_WF_TestGitHub.P01_HelloWorld" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Mi test page for Github</title>
+    
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="lblEName" runat="server" Text="Please enter your Name here:"></asp:Label>
+        <asp:TextBox ID="txtName" runat="server" AutoPostBack="True" OnTextChanged="txtName_TextChanged"></asp:TextBox><br />
+        <asp:Label ID="lblResult" runat="server" Text="Your Name is:"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ P01_WF_TestGitHub/P01_HelloWorld.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace P01_WF_TestGitHub
+{
+    public partial class P01_HelloWorld : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void txtName_TextChanged(object sender, EventArgs e)
+        {
+            if (txtName.Text.Length > 0)
+            {
+                lblResult.Text = "Your Name is: " + txtName.Text;
+                txtName.Text = string.Empty;
+                txtName.Focus();
+            }
+            else
+            {
+                txtName.Text = string.Empty;
+                txtName.Focus();
+            }
+        }
+    }
+}
